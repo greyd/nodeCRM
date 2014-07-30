dfc3c6c5910b09e5d09d565846a8d3130d8687a1
vpavlov <vpavlov@spredfast.com>
2014-07-30 20:15:42 +0300

    Configure less middleware

--- config/environment.js
+++ config/environment.js
@@ -3,19 +3,24 @@ var express  = require('express');
 var settings = require('./settings');
 var models   = require('../app/models/');
 var bodyParser = require('body-parser');
-var serveStatic = require('serve-static')
-
+var serveStatic = require('serve-static');
+var lessMiddleware = require('less-middleware');
+console.log(path.join(settings.path, 'app-ui/styles/less'));
 module.exports = function (app) {
-  app.use(serveStatic(path.join(settings.path, 'app-ui')));
-  app.use(bodyParser.json());
-  app.use(function (req, res, next) {
-    models(function (err, db) {
-      if (err) return next(err);
+	app.use(lessMiddleware('less', {
+		dest: 'css',
+		pathRoot: path.join(settings.path, 'app-ui/styles/')
+	}));
+	app.use(serveStatic(path.join(settings.path, 'app-ui')));
+	app.use(bodyParser.json());
+	app.use(function (req, res, next) {
+		models(function (err, db) {
+			if (err) return next(err);
 
-      req.models = db.models;
-      req.db     = db;
+			req.models = db.models;
+			req.db     = db;
 
-      return next();
-    });
-  });
+			return next();
+		});
+	});
 };
